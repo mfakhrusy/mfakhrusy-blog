26ac81903ba6e4e969226174c83ca9e0df491a4b
Muhamad Fakhrusy <fakhrusy.m@gmail.com>
2020-05-10 10:30:40 +0700

    migrate to typescript

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,7 +12,11 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<Props> = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
